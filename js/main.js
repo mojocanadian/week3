b2b7ba630caa43a4a19969254f1c13c7a8e3f123
Jozef Kuracina <mojocanadian@gmail.com>
2018-05-30 14:42:41 -0400

    finished js file

--- js/main.js
+++ js/main.js
@@ -17,11 +17,35 @@ console.log("Number of times visited "+num);
 var teams = ["Knights","Caps","Leafs"]; //this is a single dimension array
 console.log(teams[0]); //1st position of an array is 0
 
-function i_am_a_function() {
+var iconSet = document.querySelectorAll(".icons");
+console.log(iconSet);
+
+/*function i_am_a_function() {
 	console.log("From i_am_a_function()");
 
 }
 
 window.addEventListener("load",i_am_a_function, false);
+*/
+
+function logLoaded(evt) {
+	//console.log("svg loaded");
+	//console.log(evt);
+	var targetSVG = evt.currentTarget.contentDocument;
+	//console.log(targetSVG);
+	var theIcon = targetSVG.querySelector(".mainIcon");
+	//console.log(theIcon);
+	theIcon.addEventListener("click", logClicked, false);
+}
+
+function logClicked(evt) {
+	console.log(evt.currentTarget.id);
+}
+
+for(var i=0; i<iconSet.length; i++) {
+	//console.log(i);
+	iconSet[i].addEventListener("load", logLoaded, false);
+
+}
 
 })();
